fc5492683fbbe0b4297e4f4dd1c96a13ce65bfec
Lucas Chi <chi.lucas@gmail.com>
2012-03-22 01:40:03 -0400

    fix bug for ip address without protocol

--- freshy.js
+++ freshy.js
@@ -5,16 +5,19 @@ chrome.omnibox.onInputEntered.addListener(function(input) {
     var wsURI = 'ws://localhost:4444';
     var addr = "";
     if(splitInput.length == 1) {
-	addr = input;
+	      addr = input;
+        if(addr.match(/^\d{0,3}\.\d{0,3}\.\d{0,3}\.\d{0,3}/g)){
+          addr = "http://" + addr
+        }
     } else if(splitInput.length == 2){
-	addr = splitInput[0];
-	wsURI = "ws://" + splitInput[1];
+        addr = splitInput[0];
+        wsURI = "ws://" + splitInput[1];
     }
 
     if(addr == 'localhost') addr = 'localhost:80';
 
     var ws = new WebSocket(wsURI);
-    
+
     chrome.tabs.update(null,{
         active: true,
 	url: addr
@@ -22,7 +25,7 @@ chrome.omnibox.onInputEntered.addListener(function(input) {
         open_sockets[tab.id] = ws;
         ws.tab_id = tab.id;
     });
-    
+
     ws.onmessage = function(event) {
         console.log(event.data);
 	try {
@@ -31,7 +34,7 @@ chrome.omnibox.onInputEntered.addListener(function(input) {
             console.log(err);
         }
     }
-    
+
 });
 
 chrome.tabs.onRemoved.addListener(function(id, info) {
